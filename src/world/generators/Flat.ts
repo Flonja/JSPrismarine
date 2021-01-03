56cf11af2f924dffd1ee9f41ed541a223e2df801
Olybear9 <olybear9@gmail.com>
2021-01-02 19:32:42 -0600

    ⚡ WASM chunk generation for flat world

--- src/world/generators/Flat.ts
+++ src/world/generators/Flat.ts
@@ -1,4 +1,5 @@
 import Chunk from '../chunk/Chunk';
+import { gen_flat_chunk } from '../../utils/chunk/ChunkUtils';
 import type Server from '../../Server';
 import type Vector3 from '../../math/Vector3';
 
@@ -13,20 +14,16 @@ export default class Flat {
         server: Server;
     }) {
         const chunk = new Chunk(pos.getX(), pos.getZ());
-
         const bedrock = server.getBlockManager().getBlock('minecraft:bedrock');
         const dirt = server.getBlockManager().getBlock('minecraft:dirt');
         const grass = server.getBlockManager().getBlock('minecraft:grass');
-        for (let x = 0; x < 16; x++) {
-            for (let z = 0; z < 16; z++) {
-                let y = 0;
-                chunk.setBlock(x, y++, z, bedrock);
-                chunk.setBlock(x, y++, z, dirt);
-                chunk.setBlock(x, y++, z, dirt);
-                chunk.setBlock(x, y++, z, dirt);
-                chunk.setBlock(x, y++, z, grass);
-            }
-        }
+
+        gen_flat_chunk().forEach((block: any) => {
+            const blk = block.name === "minecraft:grass"
+                ? grass : block.name === "minecraft:dirt"
+                    ? dirt : bedrock;
+            chunk.setBlock(block.x, block.y, block.z, blk);
+        });
 
         return chunk;
     }
